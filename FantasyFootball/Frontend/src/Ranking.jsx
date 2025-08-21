7301a0e462da2cccc6e3df547a8b3e8fe84d0fcf
Connor Lilbourne <cscottlil03@gmail.com>
2025-08-20 20:59:04 -0400

    Update Ranking.jsx

--- FantasyFootball/Frontend/src/Ranking.jsx
+++ FantasyFootball/Frontend/src/Ranking.jsx
@@ -1,15 +1,19 @@
-
 import PlayerItem from "./PlayerItem";
+import adpData from "./assets/adp.json"; 
+
 function Ranking() {
-  return(<ul style={{ padding: 0 }}>
-                {team.map((player) => (
-                  <PlayerItem
-                    key={player.id}
-                    player={player}
-                    buttonLabel="Drafted"
-                    onButtonClick={() => {}}
-                  />
-                ))}
-</ul>)
+  return (
+    <ul style={{ padding: 0 }}>
+      {adpData.map((player) => (
+        <PlayerItem
+          key={player.id}
+          player={player}
+          buttonLabel="Drafted"
+          onButtonClick={() => {}}
+        />
+      ))}
+    </ul>
+  );
 }
+
 export default Ranking;
