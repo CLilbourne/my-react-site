45878116b45a96c11ece95f2981c50ab82a5fcb5
Connor Lilbourne <cscottlil03@gmail.com>
2025-08-20 21:10:27 -0400

    Update Ranking.jsx

--- FantasyFootball/Frontend/src/Ranking.jsx
+++ FantasyFootball/Frontend/src/Ranking.jsx
@@ -1,22 +1,21 @@
 import PlayerItem from "./PlayerItem";
-import adpData from "./assets/adp.json"; // example
-import MockDraft from "./MockDraft"
-function Ranking() {
+
+function Ranking({ availablePlayers, draftPickOrder, currentPickIndex, selectedTeam, draftPlayer }) {
   return (
-    <ul style={{overflowY: "auto", padding: 0}}>
-            {availablePlayers.map((player) =>
-              draftPickOrder[currentPickIndex] === selectedTeam ? (
-                <PlayerItem
-                  key={player.id}
-                  player={player}
-                  buttonLabel="Draft"
-                  onButtonClick={() => draftPlayer(player)}
-                />
-              ) : (
-                <PlayerItem key={player.id} player={player} />
-              )
-            )}
-          </ul>
+    <ul style={{ overflowY: "auto", padding: 0 }}>
+      {availablePlayers.map((player) =>
+        draftPickOrder[currentPickIndex] === selectedTeam ? (
+          <PlayerItem
+            key={player.id}
+            player={player}
+            buttonLabel="Draft"
+            onButtonClick={() => draftPlayer(player)}
+          />
+        ) : (
+          <PlayerItem key={player.id} player={player} />
+        )
+      )}
+    </ul>
   );
 }
 
