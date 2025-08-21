4cc9c4393d8bd18a1f025420d34fcfa53cfcdde5
Connor Lilbourne <cscottlil03@gmail.com>
2025-08-20 22:40:01 -0400

    Update PlayerItem.jsx

--- FantasyFootball/Frontend/src/PlayerItem.jsx
+++ FantasyFootball/Frontend/src/PlayerItem.jsx
@@ -8,18 +8,21 @@ function PlayerItem({ player, index, primaryButton, secondaryButton }) {
       style={{ display: "flex", alignItems: "center", gap: "10px" }}
     >
       {/* Number before the photo */}
-      <span style={{ width: "30px", fontWeight: "bold", textAlign: "center" }}>
-        {index}.
-      </span>
-
+      {index !== undefined && index !== null && (
+        <span style={{ width: "30px", fontWeight: "bold", textAlign: "center" }}>
+          {index}.
+        </span>
+      )}
+      {/*photo */}
       <img src={player.headshot} alt={player.fullName} />
       <div>
+      {/* Name */}
         <span style={{ fontWeight: "bold" }}>{player.fullName}</span>{" "}
         <span style={{ color: "#bbb", fontSize: "0.9em" }}>
           ({player.position}) - {player.team}
         </span>
       </div>
-
+      
       {primaryButton && (
         <button onClick={() => primaryButton.onClick(player)} style={{ marginLeft: "auto" }}>
           {primaryButton.label}
