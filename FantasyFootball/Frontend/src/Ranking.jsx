02d6c6421b706ab6a93b57fe2c3756a2a058341f
Connor Lilbourne <cscottlil03@gmail.com>
2025-08-20 22:52:23 -0400

    Update Ranking.jsx

--- FantasyFootball/Frontend/src/Ranking.jsx
+++ FantasyFootball/Frontend/src/Ranking.jsx
@@ -56,6 +56,7 @@ function Ranking() {
   };
   return (
     <div>
+    <div style={{ marginBottom: "1rem" }}></div>
       <div style={{ display: "flex", gap: 40 }}>
         <div className="draftPlayers" style={{ flex: 1 }}>
           {availablePlayers.length === 0 && <p>Server Down</p>}
