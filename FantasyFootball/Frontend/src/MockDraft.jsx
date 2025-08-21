a61da938b962db79002336a91d7cf23fd32ff96f
Connor Lilbourne <cscottlil03@gmail.com>
2025-08-20 22:37:57 -0400

    Update MockDraft.jsx

--- FantasyFootball/Frontend/src/MockDraft.jsx
+++ FantasyFootball/Frontend/src/MockDraft.jsx
@@ -218,7 +218,7 @@ draftPlayer(playerToDraft);
               draftPickOrder[currentPickIndex] === selectedTeam ? (
                 <PlayerItem
                   key={player.id}
-                  index={index + 1} // <-- This will display the number
+                  index = ""
                   player={player}
                   primaryButton={{
                    label: "Draft",
@@ -240,7 +240,7 @@ draftPlayer(playerToDraft);
                 {team.map((player) => (
                   <PlayerItem
                     key={player.id}
-                    index={index + 1} // <-- This will display the number
+                    index = ""
                     player={player}
                      primaryButton={{
                    label: "Drop",
