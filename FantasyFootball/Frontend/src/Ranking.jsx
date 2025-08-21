1f29d2623c389f3b456327cb4b686325aa63787a
Connor Lilbourne <cscottlil03@gmail.com>
2025-08-21 13:51:50 -0400

    Update Ranking.jsx

--- FantasyFootball/Frontend/src/Ranking.jsx
+++ FantasyFootball/Frontend/src/Ranking.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PlayerItem from "./PlayerItem";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import adpData from "./assets/adp.json";
 import { BACKEND_URL } from "./shared";
